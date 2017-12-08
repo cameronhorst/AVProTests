e0b0002c7d5a37ad2a57aea28f455435eda896be
Cameron <horst106@mail.chapman.edu>
2017-12-08 12:43:49 -0800

    Testing streaming

---
+++ Assets/VideoStreamer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+
+
+public class VideoStreamer : MonoBehaviour {
+
+    MediaPlayer player;
+    public string siteURL;
+    public string fileName;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GetComponent<MediaPlayer>();	
+	}
+	
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, siteURL + fileName, true);
+        }
+	}
+}
